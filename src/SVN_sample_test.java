81ea394601be3271bcdddffd6bfdc60abf318ea0
Atul Sharma <atulsharma8790@gmail.com>
2016-05-25 10:49:07 +0530

    first Commit

---
+++ src/SVN_sample_test.java
@@ -0,0 +1,15 @@
+
+public class SVN_sample_test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("This is my first SVN Sample project");
+		System.out.println("So I have learned it");
+		System.out.println("I am also learning the GIT. Thus, uploading the file from here as well");
+
+	}
+
+}
